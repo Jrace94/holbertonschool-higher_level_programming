2da6d5504193edc7e951fe88443ad38ca78ca345
Jrace94 <jfra.ing28@mgmail.com>
2023-11-12 17:51:36 -0800

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
-print(f"1 + 2 = {add_0.add(1, 2)}")
+if __name__ == "__main__":
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
