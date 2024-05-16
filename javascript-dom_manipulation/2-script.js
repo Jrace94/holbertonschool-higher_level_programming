3ca23f637b30cf6e4ace6010c52b8b108bbc7faa
Jrace94 <jfra.ing28@gmail.com>
2024-05-16 17:28:39 -0500

    Task 2: Add .red class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var redHeader = document.getElementById("red_header");
     var header = document.querySelector("header");
-
     redHeader.addEventListener("click", function () {
         header.classList.add("red");
     });
